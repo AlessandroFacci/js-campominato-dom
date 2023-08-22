1b640949f1bfda78da674f069b5032db39ffb08b
AlessandroFacci <alessandro.facci1996@gmail.com>
2023-08-22 15:36:08 +0200

    style and js: edit

--- js/main.js
+++ js/main.js
@@ -1,27 +1,33 @@
 const button = document.getElementById("generate");
 const grid = document.getElementById("grid");
 
+generateGrid();
+
 button.addEventListener("click", function () {
   generateGrid();
 });
 
 function generateGrid() {
   grid.innerHTML = "";
+
   for (let i = 1; i <= 100; i++) {
     const cell = generateCell(i);
     grid.append(cell);
   }
 }
 
-function generateCell(i) {
+function generateCell(cellText) {
   const cell = document.createElement("div");
   cell.classList.add("cell");
-  cell.innerHTML = i;
+  //cell.innerHTML = cellText;
 
   cell.addEventListener("click", function () {
     cell.classList.add("azure");
-    console.log(i);
+    console.log(cellText);
   });
 
   return cell;
 }
+
+const randomNumber = Math.floor(Math.random() * 16) + 1;
+console.log(randomNumber);
